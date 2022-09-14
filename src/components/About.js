9935c92c239cd533458349dd305a9d75614fe85c
samuelconlon <conlonsp@yahoo.com>
2022-09-13 22:48:55 -0400

    commit tree d52e9a4a9495fa34968afed279c19109ac994d9d

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,26 @@
 import React from "react";
+import Links from './Links'
 
-function About() {
+function renderP(props) {
+  if(props.bio === '') {
+    return null
+  } else if(!props.bio) {
+    return null
+  } else if(props.bio === props.bio) {
+    return <p>{props.bio}</p>
+  }
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {renderP(props)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
 
 export default About;
+
